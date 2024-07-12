a788a3bb18e38fd1c2dcbbe0e767bace744c96b6
왕성민 <wsm6753@gmail.com>
2024-07-12 11:38:50 +0900

    [fix] Flameball에 BallMove 상속시킨 후 스크립트 수정

--- Assets/Script/Unit/Player/Fireball/FlameballMove.cs
+++ Assets/Script/Unit/Player/Fireball/FlameballMove.cs
@@ -3,22 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class FlameballMove : MonoBehaviour
+public class FlameballMove : BallMove
 {
-    private Rigidbody _rb;
-
-    private IObjectPool<FlameballMove> _flameballPool;
-
-    private Vector3 _direction;
-
-    private float _bulletSpeed;
-    private float _bulletLifeTime;
-
-    private bool _isDestroyed;
-
-    private void Awake()
+    protected override void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
+        base.Awake();
 
         _bulletSpeed = 10.0f;
         _bulletLifeTime = 10.0f;
