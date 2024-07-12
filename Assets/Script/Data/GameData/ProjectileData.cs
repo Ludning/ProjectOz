d2c242ff7d86c0864ae8c65cd4e6c9b27e5c514e
강병욱 <petekbo931@gmail.com>
2024-07-12 13:17:23 +0900

    [Update]projectile테이블 public변수명 동기화

--- Assets/Script/Data/GameData/ProjectileData.cs
+++ Assets/Script/Data/GameData/ProjectileData.cs
@@ -9,7 +9,7 @@ public class ProjectileData
     public string projectileSpeedRate;
     public float projectileLifeTime;
     public string projectileRemoveLandSet;
-    public string 튕김_가능_횟수;
+    public string projectileBounceCount;
     public string projectileRemoveTrigger2;
     public string projectileRemoveTrigger3;
     public string projectileTarget;
