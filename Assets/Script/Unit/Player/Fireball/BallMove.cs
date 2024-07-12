49691e37b5f52b9df18cf093d6ddd95c17de92fa
왕성민 <wsm6753@gmail.com>
2024-07-12 11:37:55 +0900

    [Add] Fireball과 Flameball을 상속시킬 BallMove 스크립트 추가

---
+++ Assets/Script/Unit/Player/Fireball/BallMove.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class BallMove : MonoBehaviour
+{
+    protected Rigidbody _rb;
+
+    protected IObjectPool<BallMove> _ballPool;
+
+    protected Vector3 _direction;
+
+    protected float _bulletSpeed;
+    protected float _bulletLifeTime;
+
+    protected bool _isDestroyed;
+
+    protected virtual void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    protected virtual void OnEnable()
+    {
+        _isDestroyed = false;
+        CancelInvoke("DestroyBall"); 
+    }
+
+    protected void OnDisable()
+    {
+        _rb.velocity = Vector3.zero;
+    }
+
+    public void Shoot()
+    {
+        _direction = transform.forward;
+        _rb.velocity = _direction * _bulletSpeed;
+        Invoke("DestroyFireball", _bulletLifeTime);
+    }
+
+    public void SetManagedPool(IObjectPool<BallMove> ballPool)
+    {
+        _ballPool = ballPool;
+    }
+
+    public void DestroyBall()
+    {
+        if (_isDestroyed) return;
+
+        _isDestroyed = true;
+        _ballPool.Release(this);
+    }
+}
