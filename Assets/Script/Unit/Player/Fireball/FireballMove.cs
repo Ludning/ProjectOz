b7f8fa86cdcb81dc33f1f68ee93d20b05a2d8a87
왕성민 <wsm6753@gmail.com>
2024-07-12 11:38:26 +0900

    [fix] FireBall에 BallMove 상속시킨 후 스크립트 수정

--- Assets/Script/Unit/Player/Fireball/FireballMove.cs
+++ Assets/Script/Unit/Player/Fireball/FireballMove.cs
@@ -3,61 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class FireballMove : MonoBehaviour
+public class FireballMove : BallMove
 {
-    private Rigidbody _rb;
-
-    private IObjectPool<FireballMove> _fireballPools;
-
-    private Vector3 _direction;
-
-    private float _bulletSpeed;
-    private float _bulletLifeTime;
-
     private int _maxBounceCount;
     private int _bounceCount;
 
-    private bool _isDestroyed;
-
-    private void Awake()
+    protected override void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
+        base.Awake();
 
         _bulletSpeed = 10.0f;
         _bulletLifeTime = 10.0f;
         _maxBounceCount = 2;
     }
 
-    private void OnEnable()
+    protected override void OnEnable()
     {
+        base.OnEnable();
         _bounceCount = 0;
-        _isDestroyed = false;
-        CancelInvoke("DestroyFireball");
-    }
-
-    private void OnDisable()
-    {
-        _rb.velocity = Vector3.zero;
-    }
-
-    public void Shoot()
-    {
-        _direction = transform.forward;
-        _rb.velocity = _direction * _bulletSpeed;
-        Invoke("DestroyFireball", _bulletLifeTime);
-    }
-
-    public void SetManagedPool(IObjectPool<FireballMove> fireballPool)
-    { 
-        _fireballPools = fireballPool;
-    }
-
-    public void DestroyFireball()
-    {
-        if (_isDestroyed) return;
-
-        _isDestroyed = true;
-        _fireballPools.Release(this);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -70,10 +33,9 @@ public class FireballMove : MonoBehaviour
 
             if ( _bounceCount == _maxBounceCount )
             {
-                DestroyFireball();
+                DestroyBall();
             }
 
-            //_rb.AddForce(Vector3.up * 15f, ForceMode.Impulse);
             _rb.velocity = new Vector3(_rb.velocity.x, 10.0f, _rb.velocity.z);
         }
     }
