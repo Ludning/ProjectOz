20bbe652c134c8b0cf343cdd769e85c2a4240998
왕성민 <wsm6753@gmail.com>
2024-07-15 18:10:33 +0900

    [fix] 메테오 데이터 연결

--- Assets/Script/OzMagic/Projectile_Meteor.cs
+++ Assets/Script/OzMagic/Projectile_Meteor.cs
@@ -3,15 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile_Meteor : MonoBehaviour
+public class Projectile_Meteor : OzMagic
 {
+    private OzmagicData _meteorData_OzMagic;
+    private ProjectileData _meteorData_Projectile;
+
     private Rigidbody _rb;
 
     private Camera _camera;
 
     private Vector3 _spawnPos;
 
-    private float _lifeTime;
     private float _damageTimer;
     private float _hitCycle;
     private float _meteorDamage;
@@ -20,9 +22,15 @@ public class Projectile_Meteor : MonoBehaviour
 
     private void Awake()
     {
+        _meteorData_OzMagic = DataManager.Instance.GetGameData<OzmagicData>("O101");
+        _meteorData_Projectile = DataManager.Instance.GetGameData<ProjectileData>("P201");
+
         _rb = GetComponent<Rigidbody>();
-        _lifeTime = 10.0f;
-        _gravity = -4.0f;
+
+        _ozMagicPercentage = _meteorData_OzMagic.ozSkillPercentage;
+
+        _lifeTime = _meteorData_Projectile.projectileLifeTime;
+        _gravity = _meteorData_Projectile.projectileSpeedRate;
         _rotSpeed = 50.0f;
     }
 
@@ -32,7 +40,7 @@ public class Projectile_Meteor : MonoBehaviour
         _camera = Camera.main;
         _spawnPos = _camera.gameObject.transform.position + new Vector3(0.0f, _camera.orthographicSize, 10.0f);
         transform.position = _spawnPos;
-        _rb.velocity = new Vector3(0, _gravity, 0);
+        _rb.velocity = new Vector3(0, -_gravity, 0);
         _damageTimer = 0f;
         Invoke(nameof(DestroyMeteor), _lifeTime);
     }
@@ -40,6 +48,8 @@ public class Projectile_Meteor : MonoBehaviour
     private void Update()
     {
         transform.Rotate(new Vector3(0, 0, _rotSpeed * Time.deltaTime));
+        Debug.Log(_ozMagicPercentage);
+        Debug.Log(_lifeTime);
     }
 
     private void DestroyMeteor()
