902a902c24a48662d19529d9380c1af249458508
변석진 <bsj2234@naver.com>
2024-07-15 09:46:24 +0900

    [rename] 폴더정리

--- Assets/Scripts/IKnockbackAble.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IKnockbackAble 
-{
-    public void KnockbackOnSurface(Vector3 direction, float force);
-}
