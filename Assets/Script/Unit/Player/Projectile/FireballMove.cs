d72631f9c5d03613ccc506370b41ddf7ce4c351a
왕성민 <wsm6753@gmail.com>
2024-07-16 18:06:45 +0900

    [Update]

--- Assets/Script/Unit/Player/Projectile/FireballMove.cs
+++ Assets/Script/Unit/Player/Projectile/FireballMove.cs
@@ -39,19 +39,6 @@ public class FireballMove : BallMove
 
     private void OnTriggerEnter(Collider other)
     {
-        Vector3 collisionPoint = other.ClosestPoint(transform.position);
-        
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, (collisionPoint - transform.position).normalized, out hit))
-        {
-            Vector3 normal = hit.normal;
-            CheckNormalDirection(normal);
-        }
-        else
-        {
-            Debug.Log("이런");
-        }
-
         if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform") || other.gameObject.CompareTag("Wall"))
         {
             _bounceCount++;
@@ -61,10 +48,11 @@ public class FireballMove : BallMove
                 DestroyBall();
             }
 
-            //if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform"))
-            //    _rb.velocity = new Vector3(_rb.velocity.x, 4.0f + _gravityValue, _rb.velocity.z) + _gravity;
-            //else
-            //    _rb.velocity = new Vector3(-_rb.velocity.x, _rb.velocity.y, -_rb.velocity.z);
+            //if (other.gameObject.CompareTag("ground") || other.gameObject.CompareTag("platform"))
+            if (transform.position.y - other.gameObject.transform.position.y > other.gameObject.transform.localScale.y / 2f - 0.15f)
+                _rb.velocity = new Vector3(_rb.velocity.x, 4.0f + _gravityValue, _rb.velocity.z) + _gravity;
+            else
+                _rb.velocity = new Vector3(-_rb.velocity.x, _rb.velocity.y, -_rb.velocity.z);
         }
         if (other.CompareTag("Enemy"))
         {
@@ -72,25 +60,6 @@ public class FireballMove : BallMove
             {
                 combat.Damaged(_damage);
             }
-        }
-    }
-    void CheckNormalDirection(Vector3 normal)
-    {
-        // 각 방향 벡터와의 코사인 유사도를 계산
-        float dotUp = Vector3.Dot(normal, Vector3.up);
-        float dotLeft = Vector3.Dot(normal, Vector3.left);
-        float dotRight = Vector3.Dot(normal, Vector3.right);
-
-        // 가장 높은 코사인 유사도를 가진 방향을 찾음
-        float maxDot = Mathf.Max(dotUp, dotLeft, dotRight);
-
-        if (maxDot == dotUp)
-        {
-            _rb.velocity = new Vector3(_rb.velocity.x, 4.0f + _gravityValue, _rb.velocity.z) + _gravity;
-        }
-        else if (maxDot == dotLeft || maxDot == dotRight)
-        {
-            _rb.velocity = new Vector3(-_rb.velocity.x, _rb.velocity.y, -_rb.velocity.z);
         }
     }
 }
