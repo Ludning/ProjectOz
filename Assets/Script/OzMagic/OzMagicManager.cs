e338b558cf2bb68acaac3fe6af3f0a1a30c38129
왕성민 <wsm6753@gmail.com>
2024-07-16 13:33:26 +0900

    [feat] 메테오 오브젝트 풀 연결 및 오즈매직 가중치 랜덤 알고리즘 적용

--- Assets/Script/OzMagic/OzMagicManager.cs
+++ Assets/Script/OzMagic/OzMagicManager.cs
@@ -3,61 +3,93 @@ using UnityEngine;
 using UnityEngine.Pool;
 
 public class OzMagicManager : SingleTonMono<OzMagicManager>
-{ 
-    private OzmagicData _ozData;
-    private OzMagic _ozMagic;
+{
+    private IObjectPool<OzMagic> _meteorPool;
 
     [SerializeField] private List<GameObject> Prefab_OzMagic = new List<GameObject>();
     [SerializeField] private List<float> _ozMagic_weights;
-    private int _listIndex = 0;
+
 
     private int _ozMagicIndex;
 
     private void Awake()
     {
         Prefab_OzMagic.Add(ResourceManager.Instance.LoadResource<GameObject>("Meteor"));
-    }
-
-    private void OnEnable()
-    {
-        Execute();
 
         _ozMagic_weights = new List<float>(new float[Prefab_OzMagic.Count]);
-        foreach (var item in Prefab_OzMagic)
-        {
-            var oz = item.GetComponent<OzMagic>();
-            _ozMagic_weights[_listIndex] = oz._ozMagicPercentage;
-            _listIndex++;
-        }
-    }
 
-    private void Update()
-    {
-        Debug.Log(_ozMagic_weights[0]);
+        for (int i = 0; i < Prefab_OzMagic.Count; i++)
+        {
+            var oz = Prefab_OzMagic[i].GetComponent<OzMagic>();
+            _ozMagic_weights[i] = oz._ozMagicPercentage;
+        }
+        //_ozMagic_weights.Add(1f);
+
+        _meteorPool = new ObjectPool<OzMagic>(() => CreateBall(Prefab_OzMagic[0], _meteorPool), OnGetBall, OnReleaseBall, OnDestroyBall);
     }
 
     public void Execute()
     {
-        //RandomOzMagic();
-        Instantiate(Prefab_OzMagic[_ozMagicIndex]);
+        RandomOzMagic();
+        switch (_ozMagicIndex)
+        {
+            case 0:
+                OnExcute(_meteorPool);
+                break;
+        }      
     }
 
     private void RandomOzMagic()
     {
-        _ozMagicIndex = Random.Range(0, Prefab_OzMagic.Count);
+        _ozMagicIndex = GetRandomByWeight(_ozMagic_weights);
     }
 
-    private int GetRandomByWeight(List<int> weight)
+    private int GetRandomByWeight(List<float> weight)
     {
-        int totalWeight = 0;
+        float totalWeight = 0;
 
         foreach (var i in weight)
         {
             totalWeight += i;
         }
 
+        float randomValue = Random.Range(0, totalWeight);
+        float accumulatedWeight = 0f;
 
+        for (int i = 0; i < weight.Count; i++)
+        {
+            accumulatedWeight += weight[i];
+            if (randomValue < accumulatedWeight)
+            {
+                return i;
+            }
+        }
 
         return 0;
+    }
+
+    private void OnExcute(IObjectPool<OzMagic> ozPool)
+    {
+        var oz = ozPool.Get();
+        oz.Excute();
+    }
+
+    private OzMagic CreateBall(GameObject prefab, IObjectPool<OzMagic> ozPool)
+    {
+        var oz = Instantiate(prefab, transform.position, transform.rotation).GetComponent<OzMagic>();
+        oz.SetManagedPool(ozPool);
+        return oz;
+    }
+    private void OnGetBall(OzMagic oz)
+    {
+        oz.gameObject.SetActive(true);
+    }
+    private void OnReleaseBall(OzMagic oz)
+    {
+        oz.gameObject.SetActive(false);
+    }
+    private void OnDestroyBall(OzMagic oz)
+    {
+        Destroy(oz.gameObject);
     }
 }
