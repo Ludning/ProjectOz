5054608693417187be61df3da2a97953d5fa7510
왕성민 <wsm6753@gmail.com>
2024-07-19 04:07:17 +0900

    [Add] 메테오가 폭발하는 스크립트 생성

---
+++ Assets/Script/Unit/Player/Projectile/Explosion/MeteorExplosion.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class MeteorExplosion : MonoBehaviour
+{
+    private OzmagicData _meteorData_OzMagic;
+
+    IObjectPool<MeteorExplosion> pool;
+
+    private Vector3 _upScale;
+
+    private float _explosionRadius;
+    private float _explosionDamage;
+    [SerializeField] private float _upScaleValue;
+
+    private bool _isDestroyed = false;
+    public bool isPlayerHit;
+
+    private void Awake()
+    {
+        _meteorData_OzMagic = DataManager.Instance.GetGameData<OzmagicData>("O101");
+        _explosionRadius = _meteorData_OzMagic.value3;
+        _explosionDamage = _meteorData_OzMagic.chainPowerRate;
+
+        _upScale = new Vector3(_upScaleValue, _upScaleValue, 0f) * Time.deltaTime;
+    }
+
+    private  void OnEnable()
+    {
+        _isDestroyed = false;
+    }
+
+    private void Update()
+    {
+        transform.localScale += _upScale;
+
+        if (transform.localScale.x >= _explosionRadius)
+        {
+            DestroyExplosion();
+        }
+    }
+
+    public void Explosion()
+    { 
+        
+    }
+
+    public void SetManagedPool(IObjectPool<MeteorExplosion> Pool)
+    {
+        pool = Pool;
+    }
+    public void DestroyExplosion()
+    {
+        if (_isDestroyed) return;
+
+        _isDestroyed = true;
+        pool.Release(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            if (other.TryGetComponent(out Combat combat))
+            {
+                combat.Damaged(_explosionDamage);
+            }
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (isPlayerHit) return;
+
+            if (other.TryGetComponent(out Combat combat))
+            {
+                combat.Damaged(_explosionDamage);
+            }
+        }
+    }
+}
